a5fd25da093ec441c6a658af8fe81dd2d7dc0e7f
yohan <yohan@yoh-PC>
2017-04-16 02:35:40 +0200

    add->installer.php: 	intalador de composer (php installer.php)dentro del directorio raiz 		de symfony. (php composer install) para instalar proyecto.

--- src/AppBundle/Form/ReservaType.php
+++ src/AppBundle/Form/ReservaType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Blank;
 
